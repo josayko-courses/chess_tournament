70d437a592d90eb00145d7164a5479efe88f7ac1
Jonny Saykosy <josayko@pm.me>
2021-09-20 14:00:58 +0200

    ok rating choice loop

--- chess_tournament/views/init_form.py
+++ chess_tournament/views/init_form.py
@@ -3,6 +3,9 @@
 """
 
 
+from typing import NamedTuple
+
+
 def get_input():
     """Get user input and return the data as a dictionary"""
     # Rating choices, default is 'rapid'
@@ -13,10 +16,16 @@ def get_input():
     name = input("Enter tournament's name: ")
     location = input("Enter tournament's location: ")
 
-    # Rating choice
-    rating = input("Rating type :\n    1 rapid\n    2 blitz\n    3 bullet\n    Rating ? (1-3, default=1) ")
-    if int(rating) > 0 and int(rating) <= 3:
-        option = int(option) - 1
+    # Rating choice. Loop until user enter a valid option
+    while True:
+        rating = input("Rating type :\n    1 rapid\n    2 blitz\n    3 bullet\n    Rating ? (1-3, default=1) ")
+        try:
+            rating = int(rating)
+            if rating > 0 and rating <= 3:
+                option = int(option) - 1
+                break
+        except ValueError:
+            continue
 
     # End date
     end = input("Enter length of the tournament : (days) ")
