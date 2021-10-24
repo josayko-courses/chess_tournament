b07dbd507275cb5b928db0711105df4ed7af42fd
Jonny Saykosy <josayko@pm.me>
2021-10-24 20:35:30 +0200

    Ok update game results locally. TODO: update DB

--- chess_tournament/controllers/round_manager.py
+++ chess_tournament/controllers/round_manager.py
@@ -6,5 +6,17 @@ from controllers import App
 
 
 class RoundManager:
-    def edit_results():
-        print('edit results')
+    def update_results(dirname, result, game):
+        if result == 1:
+            game[0][1] = 1
+            game[1][1] = 0
+        elif result == 2:
+            game[0][1] = 0
+            game[1][1] = 1
+        elif result == 3:
+            game[0][1] = 0.5
+            game[1][1] = 0.5
+        elif result == 4:
+            game[0][1] = 0
+            game[1][1] = 0
+        return
--- chess_tournament/views/round_ui.py
+++ chess_tournament/views/round_ui.py
@@ -2,7 +2,7 @@
 
 """
 
-from controllers import App
+from controllers import App, RoundManager
 from bcolors import Color
 
 
@@ -26,15 +26,30 @@ class RoundUI:
             game_index = input("Game nb. ? ")
             try:
                 game_index = int(game_index) - 1
-                if game_index < 0 or game_index > last_index:
+                if game_index < -1 or game_index > last_index:
                     continue
                 break
             except ValueError:
                 continue
         return game_index
 
-    def select_result(self):
-        return
+    def select_result(self, game_index, g):
+        while True:
+            print(f"{Color.WARNING}Edit results for Game {game_index + 1}{Color.ENDC}: ")
+            print(f"    [1] id: {g[0][0].id}, {g[0][0].surname} {g[0][0].name} {Color.OKGREEN}win{Color.ENDC}")
+            print(f"    [2] id: {g[1][0].id}, {g[1][0].surname} {g[1][0].name} {Color.OKGREEN}win{Color.ENDC}")
+            print("    [3] Draw game")
+            print("    [4] Reset results")
+            print("    [0] Cancel")
+            result = input("Result ? ")
+            try:
+                result = int(result)
+                if result < 0 or result > 4:
+                    continue
+                break
+            except ValueError:
+                continue
+        return result
 
     def get_games_with_players_instance(self, game, players):
         detailed_game = []
@@ -49,26 +64,31 @@ class RoundUI:
         return detailed_game
 
     def edit(self, tournament):
-        print("+++++++ Edit round ++++++++")
         if len(tournament.rounds) == 0:
             return print(f"{Color.FAIL}Please start tournament first{Color.FAIL}")
         elif tournament.rounds[-1].end:
             return print(f"{Color.FAIL}The tournament is over{Color.FAIL}")
 
         games = tournament.rounds[-1].games
-        last_index = self.print_games(tournament, games)
-        game_index = self.select_game(last_index)
 
-        game = games[game_index]
-        players = tournament.get_players_instance(App.players)
-        g = self.get_games_with_players_instance(game, players)
-        print(f"{Color.WARNING}Edit results for Game {game_index + 1}{Color.ENDC}: ")
-        print(f"[1] id: {g[0][0].id}, {g[0][0].surname} {g[0][0].name} {Color.OKGREEN}win{Color.ENDC}")
-        print(f"[2] id: {g[1][0].id}, {g[1][0].surname} {g[1][0].name} {Color.OKGREEN}win{Color.ENDC}")
-        print("[3] Draw game")
-        print("[4] Reset results")
-        print("[0] Cancel")
-        return
+        while True:
+            print("+++++++ Edit round ++++++++")
+            print(f"{Color.WARNING}Select Game nb. or nb. [0] to Cancel...{Color.ENDC}")
+            last_index = self.print_games(tournament, games)
+            game_index = self.select_game(last_index)
+            if game_index == -1:
+                return -1
+
+            players = tournament.get_players_instance(App.players)
+            g = self.get_games_with_players_instance(games[game_index], players)
+            result = self.select_result(game_index, g)
+            if result != 0:
+                print(games[game_index])
+                RoundManager.update_results(self.dirname, result, games[game_index])
+                print(games[game_index])
+                print(f"{Color.OKGREEN}Results updated{Color.ENDC}")
+                input(f"{Color.OKBLUE}Press ENTER to continue...{Color.ENDC}")
+                continue
 
     def terminate(self, tournament):
         print("+++++++ Terminate round ++++++++")
