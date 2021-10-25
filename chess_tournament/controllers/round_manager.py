88bbf65c3e9672069233c06e4c37dc8c3a6d4a6f
Jonny Saykosy <josayko@pm.me>
2021-10-25 11:43:40 +0200

    Move updated game results in Database class

--- chess_tournament/controllers/round_manager.py
+++ chess_tournament/controllers/round_manager.py
@@ -48,40 +48,5 @@ class RoundManager:
 
         # Update db
         db = Database(dirname)
-        for t in db.tournaments:
-            if t.doc_id == tournament.id:
-                break
-
-        # Edit game
-        updated_games = []
-        for i, g in enumerate(t['rounds'][-1]['games']):
-            if i == game_index:
-                updated_games.append(game)
-            else:
-                updated_games.append(g)
-
-        # Edit round with updated game
-        updated_rounds = []
-        for i, r in enumerate(t['rounds']):
-            if i == len(t['rounds']) - 1:
-                serialized_round = {'name': r['name'], 'start': r['start'], 'end': r['end'], 'games': updated_games}
-                updated_rounds.append(serialized_round)
-            else:
-                updated_rounds.append(r)
-
-        # Edit players total score
-        updated_players = []
-        for p in t['players']:
-            if p[0] == player1[0]:
-                updated_players.append(player1)
-            elif p[0] == player2[0]:
-                updated_players.append(player2)
-            else:
-                updated_players.append(p)
-
-        # Save to db
-        db.tournaments.update(
-            {'rounds': updated_rounds, 'players': updated_players},
-            doc_ids=[tournament.id],
-        )
+        db.update_results(tournament, game, game_index, player1, player2)
         return
